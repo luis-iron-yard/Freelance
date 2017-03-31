546ae173d565c2e4217f274f1e5cba79378fa938
llancon <rdzalb@gmail.com>
2017-03-31 12:08:07 -0400

    Testing fixes to contact js

--- js/contact_me.js
+++ js/contact_me.js
@@ -22,16 +22,16 @@ $(function() {
             }
             $.ajax({
                 url: "https://formspree.io/rdzalb@gmail.com",
-                type: "POST",
-                headers : {
-                    Accept : 'application/json'
-                      },
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                    dataType: json
+                method: "POST",
+                // headers : {
+                //     Accept : 'application/json'
+                //       },
+                data:  $(this).serialize(),
+                    // name: name,
+                    // phone: phone,
+                    // email: email,
+                    // message: message,
+                dataType: json
                 },
                 cache: false,
                 success: function() {
@@ -41,7 +41,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
